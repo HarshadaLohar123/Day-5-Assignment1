3d4b6205dc63e5a319c042c327a1f7d39dc9450c
HarshadaLohar123 <harshalohar123@gmail.com>
2022-02-24 18:10:38 +0530

    ADDED:Two digit value and find their sum and the average

---
+++ sumAvg2DigitNo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x 
+
+no1=$((RANDOM%100))
+no2=$((RANDOM%100))
+no3=$((RANDOM%100))
+no4=$((RANDOM%100))
+no5=$((RANDOM%100))
+
+sum=$(( $no1 + $no2 + $no3 + $no4 + $no5 ))
+avg=$(( $sum / 5 ))
+echo "Sum : " $sum "   Average :  " $avg 
